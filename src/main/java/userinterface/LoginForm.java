46428ae3f03a7117bb6e362acb34d7f4dd8fd131
draganoid16 <draganoid5@live.com>
2022-12-22 16:47:07 +0000

    loginForm adicionado (parte grafica)

---
+++ src/main/java/userinterface/LoginForm.java
@@ -0,0 +1,30 @@
+package userinterface;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginForm extends JDialog{
+    private JTextField emailField;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+    private JPanel loginPanel;
+    private JLabel restaurantImage;
+    private JLabel loginImage;
+
+
+    public LoginForm(JFrame parent) {
+        super(parent);
+        setTitle("Order-N-Go Login");
+        setContentPane(loginPanel);
+        setMinimumSize(new Dimension(750, 750));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+
+    private void createUIComponents() {
+        restaurantImage = new JLabel(new ImageIcon("C:\\Users\\Joao\\Desktop\\Order-N-Go\\src\\food-tray.png"));
+        loginImage = new JLabel(new ImageIcon("C:\\Users\\Joao\\Desktop\\Order-N-Go\\src\\login.png"));
+    }
+}
